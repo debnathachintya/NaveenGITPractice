01f1c4a79a4a0faa134ea3f1bb66bfecf4d464e8
snnydebnath <snny.debnath@gmail.com>
2024-09-13 23:06:19 +0530

    test

--- src/main/java/NaveenGITPractice/NaveenGITPractice/OrderPage.java
+++ src/main/java/NaveenGITPractice/NaveenGITPractice/OrderPage.java
@@ -5,6 +5,7 @@ public class OrderPage {
 	public static void main(String[] args) {
 		System.out.println("Order Page");
 		System.out.println("Test");
+		System.out.println("Order Page Commit");
 	}
 
 }
